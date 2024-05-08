a6ff784a4cc8ee119f92c5fbaf9735ca5e3fed7f
sliterok <12751644+sliterok@users.noreply.github.com>
2024-05-08 18:35:47 +0200

    add more reasonable logic for weekend sleep times

--- src/backend/night.ts
+++ src/backend/night.ts
@@ -23,6 +23,7 @@ const wakeupTimeWeekend = getTime(10, 0, true)
 const sleepTimeWeekend = getTime(23, 58, true)
 
 export function startNightChecks() {
+	updateNightStatus()
 	for (const { minutes, hours, time } of nightUpdateTimes) {
 		CronJob.from({
 			cronTime: `${minutes} ${hours} * * *`,
@@ -33,25 +34,65 @@ export function startNightChecks() {
 	}
 }
 
-function updateNightStatus(time: number) {
-	const d = new Date()
-	const day = d.getDay()
-	const isWeekend = [6, 7].includes(day)
-
-	if (time >= (isWeekend ? sleepTimeWeekend : sleepTime)) dynamic.isNight = true
-	else if (time <= (isWeekend ? wakeupTimeWeekend : wakeupTime)) dynamic.isNight = true
+function updateNightStatus(time = getCurrentTime()) {
+	const targetSleepTime = getTargetTime(TargetTimes.sleep)
+	const targetWakeupTime = getTargetTime(TargetTimes.wakeUp)
+	if (time >= targetSleepTime) dynamic.isNight = true
+	else if (time <= targetWakeupTime) dynamic.isNight = true
 	else {
 		dynamic.isNight = false
 		return
 	}
 
-	updateDisabledColor(time, isWeekend)
+	updateDisabledColor(time, targetWakeupTime)
 }
 
-function updateDisabledColor(time: number, isWeekend: boolean) {
-	if (time >= preWakeupTime && time <= (isWeekend ? wakeupTimeWeekend : wakeupTime)) {
+function updateDisabledColor(time: number, targetWakeupTime: number) {
+	if (time >= preWakeupTime && time <= targetWakeupTime) {
 		if (dynamic.disabledColor[1] !== 1) dynamic.disabledColor = [0, 1, 0]
 	} else {
 		if (dynamic.disabledColor[2] !== 1) dynamic.disabledColor = [0, 0, 1]
 	}
+}
+
+enum TargetTimes {
+	sleep,
+	wakeUp,
+}
+
+enum WekeendDay {
+	workday,
+	friday,
+	saturday,
+	sunday,
+}
+
+function getTargetTime(target: TargetTimes): number {
+	const isWeekend = getIsWeekend()
+	if (target === TargetTimes.wakeUp) {
+		return isWeekend !== WekeendDay.friday ? wakeupTimeWeekend : wakeupTime
+	} else if (target === TargetTimes.sleep) {
+		return isWeekend !== WekeendDay.sunday ? sleepTimeWeekend : sleepTime
+	}
+	throw Error(`unexpected target: ${target} in getTargetTime`)
+}
+
+const weekends = new Map([
+	[5, WekeendDay.friday],
+	[6, WekeendDay.saturday],
+	[0, WekeendDay.sunday],
+])
+
+function getIsWeekend() {
+	const d = new Date()
+	const day = d.getDay()
+	const isWeekend = weekends.get(day)
+	return isWeekend || WekeendDay.workday
+}
+
+function getCurrentTime() {
+	const d = new Date()
+	const time = getTime(d.getHours(), d.getMinutes())
+
+	return time
 }
