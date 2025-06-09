51903658387b800bba96ba6948fa71009bfb7be7
sliterok <12751644+sliterok@users.noreply.github.com>
2025-06-09 20:59:25 +0200

    export router for zenom

--- server/index.ts
+++ server/index.ts
@@ -1,26 +1,24 @@
-import express from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url'
+import express, { Router } from 'express'
 import { init } from '../src/backend'
 import { settings } from '../src/settings'
 import { isVerifiedUser } from '../src/backend/telegram/verify'
 import { IArrColor } from '../src/typings'
 import { streamHandler } from '../src/routes/debug/stream.api'
 
-const app = express()
-app.use(express.json())
-
 init()
 
-app.get('/debug/stream', streamHandler)
+const router = Router()
+router.use(express.json())
 
-app.get('/api/default-color', (req, res) => {
+router.get('/debug/stream', streamHandler)
+
+router.get('/default-color', (req, res) => {
 	const [r, g, b] = settings.color
 	const a = settings.mixRatio
 	res.json({ r, g, b, a })
 })
 
-app.post('/api/color', (req, res) => {
+router.post('/color', (req, res) => {
 	const { color, alpha, tgWebAppData } = req.body as { color: IArrColor; alpha: number; tgWebAppData: string }
 	if (!tgWebAppData) return res.status(400).end()
 	if (!isVerifiedUser(tgWebAppData)) return res.status(403).end()
@@ -29,17 +27,10 @@ app.post('/api/color', (req, res) => {
 	res.status(204).end()
 })
 
-app.get('/api/alive', (req, res) => {
+router.get('/alive', (req, res) => {
 	settings.alive = new Date()
 	console.log('alive:', settings.alive)
 	res.send('ok')
 })
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-app.use(express.static(path.join(__dirname, '../client')))
-
-const port = Number(process.env.PORT) || 8001
-app.listen(port, () => {
-	console.log('Server running on port', port)
-})
+export { router }
