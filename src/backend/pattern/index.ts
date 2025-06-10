1a14ace49ea868e40b9961f0a81b6d7c98fab274
sliterok <12751644+sliterok@users.noreply.github.com>
2025-06-10 19:21:44 +0000

    Tweak transition duration

--- src/backend/pattern/index.ts
+++ src/backend/pattern/index.ts
@@ -10,7 +10,7 @@ import { getWaveColor } from './wave'
 import { getHeartbeatColor, getStrobeColor, getPulseColor } from './extra'
 import { createIndexedMapper, createFlatMapper } from './mappers'
 
-const transitionDuration = 2000
+const transitionDuration = 250
 
 function mix(a: IArrColor, b: IArrColor, t: number): IArrColor {
 	return [Math.round(a[0] + (b[0] - a[0]) * t), Math.round(a[1] + (b[1] - a[1]) * t), Math.round(a[2] + (b[2] - a[2]) * t)]
