98d1e2245c1241d7f16bcdae9307da34ee594353
sliterok <12751644+sliterok@users.noreply.github.com>
2025-06-10 09:43:23 +0200

    Smooth breathe fade

--- src/backend/pattern/breathe.ts
+++ src/backend/pattern/breathe.ts
@@ -14,7 +14,8 @@ export const getBreatheColor: IColorGetter = (_, time) => {
 	}
 
 	const hue = dynamic.breatheHue ?? baseHue
-	const amplitude = 0.25 * (1 - dynamic.overrideRatio)
+	let amplitude = 0.25 * (1 - dynamic.overrideRatio)
+	if (dynamic.overrideRatio > 0.8) amplitude = 0
 	const light = 0.5 + amplitude * Math.sin(t / 2)
 	return hslToRgb(hue, 1, light)
 }
