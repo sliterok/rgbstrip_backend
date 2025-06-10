661d667f04bcba4ad3db8ce6af73d28375e07632
sliterok <12751644+sliterok@users.noreply.github.com>
2025-06-10 10:25:09 +0200

    Better breathe effect

--- src/backend/pattern/breathe.ts
+++ src/backend/pattern/breathe.ts
@@ -2,6 +2,8 @@ import { IColorGetter } from 'src/typings'
 import { hslToRgb } from 'src/helpers'
 import { dynamic, pixelsCount } from '../shared'
 
+const phaseOffsets = Array.from({ length: pixelsCount }, () => Math.random() * Math.PI * 2)
+
 export const getBreatheColor: IColorGetter = (index, time) => {
 	const t = time / 1000
 	const baseHue = (t * 20) % 360
@@ -15,7 +17,7 @@ export const getBreatheColor: IColorGetter = (index, time) => {
 	const hue = dynamic.breatheHue ?? baseHue
 	let amplitude = 0.25 * (1 - dynamic.overrideRatio)
 	if (dynamic.overrideRatio > 0.8) amplitude = 0
-	const phase = (index / pixelsCount) * Math.PI * 2
+	const phase = phaseOffsets[index]
 	const light = 0.5 + amplitude * Math.sin(t / 2 + phase)
 	return hslToRgb(hue, 1, light)
 }
